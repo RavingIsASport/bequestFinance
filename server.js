bf242b70df4318a708865d451487cf5be679468f
Gustavo Garcia <gustavogarcia42@yahoo.com>
2024-01-04 22:17:25 -0600

    initialized the project

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const userRoutes = require("./controllers/api/userRoute");
+const db = require("./config/connection");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// import user routes
+app.use(userRoutes);
+
+db.once("open", () => {
+  app.listen(3000, () => {
+    console.log("App listening at port 3000");
+  });
+});
